17c191fb9d20fbf84ffa24e46d8e2b89a5f7b0c7
oran-gutierrez-fugon <ojgutierrez@ucdavis.edu>
2021-11-02 16:32:33 -0700

    Update hichip.R

--- hichip.R
+++ hichip.R
@@ -4,3 +4,5 @@ library("Sushi")
 #load data from fithichip
 cov <- read.table("/Users/fsociety/Documents/GitHub/angelman_ctcf/output_tables/5k.coverage.bedgraph")
 arc <- read.table("/Users/fsociety/Documents/GitHub/angelman_ctcf/output_tables/CTCF.DS.5kb.interactions_FitHiC_Q0.10_MergeNearContacts.bed", header=TRUE)
+
+head(arc)
