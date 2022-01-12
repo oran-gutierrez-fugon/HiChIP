a513574339277e7cb2cae180d167b1029b076c18
Oran Gutiérrez Fugón <ojgutierrez@ucdavis.edu>
2022-01-12 10:36:23 -0800

    Hi-ChIP pipeline
    Modified Dovetail pipeline for processing Hi-ChIP data on UC Davis cluster

--- dovetailLUHMES.sh
+++ dovetailLUHMES.sh
@@ -45,41 +45,11 @@ samtools index mapped.PT.bam; echo "#7 samtools index bam file" | mail -s "#7 sa
 #corrected -view removed dash and input file namento mapped.PT.bam
 samtools view -h -F 0x900 mapped.PT.bam | bedtools bamtobed -i stdin > prefix.primary.aln.bed
 
-#7.6 Call peaks using MACS2
-Macs2 callpeak –t prefix.primary.aln.bed -n prefix.macs2; echo "#7.6 Macs2" | mail -s "#7.6 Macs2" ojg333@gmail.com
-
+#7.6 Call peaks using MACS3 (MACS2 was not able to run on UCDavis cluster)
+macs3 callpeak –t prefix.primary.aln.bed -n prefix.macs3; echo "#7.6 Macs3" | mail -s "#7.6 Macs3" ojg333@gmail.com
 
 #8 Library QC
 python3 ./HiChiP/get_qc.py -p stats.txt
 
-#results for neurons
-#Total Read Pairs                              703,455,028  100%
-#Unmapped Read Pairs                           34,213,851   4.86%
-#Mapped Read Pairs                             570,472,581  81.1%
-#PCR Dup Read Pairs                            256,466,690  36.46%
-#No-Dup Read Pairs                             314,005,891  44.64%
-#No-Dup Cis Read Pairs                         241,703,113  76.97%
-#No-Dup Trans Read Pairs                       72,302,778   23.03%
-#No-Dup Valid Read Pairs (cis >= 1kb + trans)  178,847,900  56.96%
-#No-Dup Cis Read Pairs < 1kb                   135,157,991  43.04%
-#No-Dup Cis Read Pairs >= 1kb                  106,545,122  33.93%
-#No-Dup Cis Read Pairs >= 10kb                 48,968,925   15.59%
-
-undif
-Total Read Pairs                              1,200,050,143  100%
-Unmapped Read Pairs                           49,185,152     4.1%
-Mapped Read Pairs                             1,002,756,649  83.56%
-PCR Dup Read Pairs                            379,279,444    31.61%
-No-Dup Read Pairs                             623,477,205    51.95%
-No-Dup Cis Read Pairs                         498,385,368    79.94%
-No-Dup Trans Read Pairs                       125,091,837    20.06%
-No-Dup Valid Read Pairs (cis >= 1kb + trans)  369,337,159    59.24%
-No-Dup Cis Read Pairs < 1kb                   254,140,046    40.76%
-No-Dup Cis Read Pairs >= 1kb                  244,245,322    39.17%
-No-Dup Cis Read Pairs >= 10kb                 101,494,724    16.28%
-
-
 #9 ChIP Enrichment Stats
 ./HiChiP/enrichment_stats.sh -g hg38.genome -b mapped.PT.bam -p /share/lasallelab/Oran/dovetail/ENCFF017XLW.bed -t 50 -x CTCF; echo "#9 ChIP Enrichment Stats" | mail -s "#9 ChIP Enrichment Stats" ojg333@gmail.com
-
-
