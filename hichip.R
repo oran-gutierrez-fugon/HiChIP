15f7b3c38dbf2031419019eeaa96886d492d4f6e
oran-gutierrez-fugon <ojgutierrez@ucdavis.edu>
2022-03-01 15:46:35 -0800

    Update hichip.R

--- hichip.R
+++ hichip.R
@@ -1,14 +1,14 @@
 #load libraries
 library("Sushi")
 #load data from fithichip
-arc <- read.table("/Volumes/DOVETAIL/UndifConcat/UndifConcat01_Merge_Nearby_Interactions/CTCF.DS.5kb.interactions_FitHiC_Q0.01_MergeNearContacts.bed", header=TRUE)
+arc <- read.table("/Volumes/DOVETAIL/NeuronsConcat/catbalanced/UDP4_01_Merge_Nearby_Interactions/CTCF.DS.5kb.interactions_FitHiC_Q0.01_MergeNearContacts.bed", header=TRUE)
 #adds distance column
 arc$dist <- abs(arc$e2 - arc$s1)
 
-#Set Region to visualize (Angelman locus 15q.11-q13 in Lymphoblast GM12878) #dovetail sample had chr8:22500000-23200000
+#Set Region to visualize (Angelman locus 15q.11-q13)
 chrom = "chr15"
-chromstart = 24718748
-chromend = 26143749
+chromstart = 25120000
+chromend = 25950000
 #Plot arcs with arc heights based on contact frequency
 plotBedpe(arc,chrom,chromstart,chromend,heights = arc$sumCC,plottype="loops", flip=TRUE)
 labelgenome(chrom, chromstart,chromend,side=3, n=3,scale="Mb")
