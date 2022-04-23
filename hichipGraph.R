8a2cf7e3edc2ead77a71733b6b5e11196218f474
oran-gutierrez-fugon <ojgutierrez@ucdavis.edu>
2022-04-22 17:21:16 -0700

    Fixed duplicate removal error

--- hichipGraph.R
+++ hichipGraph.R
@@ -1,43 +1,43 @@
 #load libraries
 library("Sushi")
 library("biomaRt")
-#load data from fithichip
-arc <- read.table("/Users/fsociety/Library/CloudStorage/OneDrive-UCDavis/Documents/Dovetailoutput/Redos/intersectBed-10kbins01/UDP4uniqueCONCAT.bed", header=TRUE)
-#adds distance column
-arc$dist <- abs(arc$e2 - arc$s1)
+library("glue")
 
-#Set Region to visualize (Angelman locus 15q.11-q13)
-chrom = "chr15"
-chromstart = 25120000
-chromend = 25950000
-#Plot arcs with arc heights based on contact frequency
-plotBedpe(arc,chrom,chromstart,chromend,heights = arc$sumCC, plottype="loops", colorby = arc$sumCC, colorbycol=SushiColors(2), flip=FALSE)
-labelgenome(chrom, chromstart,chromend,side=1, n=12,scale="Mb")
-axis(side=2,las=2,tcl=.2)
-mtext("Contact Frequency",side=2,line=2.5,cex=1,font=2)
+#Get all the .bed files from path directory
+files <- dir(path = "/Users/fsociety/Library/CloudStorage/OneDrive-UCDavis/Documents/Dovetailoutput/Redos/intersectBed-10kbins01/noDups/",pattern = ".bed")
 
-#Trying to plot gene names as well
-'''
-
-
-useMart(biomart, dataset, host="https://www.ensembl.org",
-path="/biomart/martservice", port, archive=FALSE, version, verbose = FALSE)
-
-if(interactive()){
-
-    mart = useMart("ensembl")
-    mart=useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+for(f in files) {
+  #load data from fithichip
+  filename= glue("/Users/fsociety/Library/CloudStorage/OneDrive-UCDavis/Documents/Dovetailoutput/Redos/intersectBed-10kbins01/noDups/{f}")
+  file <- basename(filename)
+  arc <- read.table(filename, header=TRUE)
+  
+  #adds distance column
+  arc$dist <- abs(arc$e2 - arc$s1)
+  
+  #Set Region to visualize (Angelman locus 15q.11-q13)
+  chrom = "chr15"
+  chromstart = 25120000
+  chromend = 25950000
+  #Plot arcs with arc heights based on contact frequency
+  file <- sub(" ", "_", file)
+  new.file <- sub("\\..*", "",file)
+  
+  png(file=glue("/Users/fsociety/Library/CloudStorage/OneDrive-UCDavis/Documents/Dovetailoutput/Redos/intersectBed-10kbins01/noDups/{new.file}.png"),width = 1043,height = 516)
+  plotBedpe(arc,
+            chrom,
+            chromstart,
+            chromend,
+            heights = arc$sumCC, 
+            plottype="loops", 
+            colorby = arc$sumCC, 
+            colorbycol=SushiColors(2), 
+            flip=FALSE)
+  labelgenome(chrom, chromstart,chromend,side=1, n=12,scale="Mb")
+  axis(side=2,las=2,tcl=.2)
+  mtext("Contact Frequency",side=2,line=2.5,cex=1,font=2)
+  labelplot("                  10k bins FDR: 0.01", new.file, titlecex=2, titleline="1.5")
+  dev.off()
 }
 
-getBM((attributes = c("chromosome_name", "exon_chrom_start", "exon_chrom_end", "external_gene_name")))
-
-plotGenes(geneinfo = NULL, chrom = chrom, chromstart = chromstart,
-          chromend = chromend, col = SushiColors(2)(2)[1], bheight = 0.3,
-          lheight = 0.3, bentline = TRUE, packrow = TRUE, maxrows = 10000,
-          colorby = NULL, colorbyrange = NULL,
-          colorbycol = colorRampPalette(c("blue", "red")), types = "exon",
-          plotgenetype = "box", arrowlength = 0.005, wigglefactor = 0.05,
-          labeltext = TRUE, labeloffset = 0.4, fontsize = 0.7, fonttype = 2,
-          labelat = "middle")
-
-'''
+#In the future would like to plot gene names as well using useMart, getBM and plotGenees
